a0f4530c43dffd0bc23510248d4056c05da7d937
Oleg Andreiev <mr.olegandreev@gmail.com>
2022-10-06 01:20:52 +0300

    add task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
 
+const switcherBgColor = {
+  intervalId: null,
+  isActive: false,
+
+  startSwitch() {
+    if (this.isActive) {
+      return;
+    }
+    this.isActive = true;
+
+    this.intervalId = setInterval(() => {
+      body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+  },
+
+  stopSwitch() {
+    clearInterval(this.intervalId);
+    this.isActive = false;
+  },
+};
+startBtn.addEventListener('click', () => {
+  switcherBgColor.startSwitch();
+});
+stopBtn.addEventListener('click', () => {
+  switcherBgColor.stopSwitch();
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
